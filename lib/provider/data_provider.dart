d54a653d0f1564c279e362ba051fda9aec26ee38
Aylin08 <75355673+Aylin08@users.noreply.github.com>
2022-04-27 18:30:18 -0700

    final

--- lib/provider/data_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class DataProvider extends ChangeNotifier {
-  GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  bool cargandoDatos() {
-    return formKey.currentState?.validate() ?? false;
-  }
-}
